fc45bd15b1d1db7c1d71e865db1c4a428e7eefdd
jwheller0013 <jwheller0013@yahoo.com>
2025-03-08 17:48:20 -0500

    All pass but testDefaultConstructor

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,15 +7,16 @@ public class Person {
     private String name;
     private int age;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
+    public Person() {
+        Person person = new Person(name, age);
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
@@ -24,7 +25,7 @@ public class Person {
     }
 
     public void setName(String name) {
-        this.name = name
+        this.name = name;
     }
 
     public void setAge(int age) {
