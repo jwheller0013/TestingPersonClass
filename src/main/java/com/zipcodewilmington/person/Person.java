0a9bee7a0119d5d4f8610a24ff32b154c3c2fdaf
jwheller0013 <jwheller0013@yahoo.com>
2025-03-08 08:40:16 -0500

    Starting work

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,7 +7,9 @@ public class Person {
     private String name;
     private int age;
 
-    public Person() {
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public Person(int age) {
@@ -17,19 +19,23 @@ public class Person {
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
